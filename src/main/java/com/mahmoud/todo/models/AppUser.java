5de1b8fa04dc8da1a050dc0a0ff20f929ff5e6b0
mahmoud-zammit-chatti <mahmoudzammit22@gmail.com>
2025-11-16 12:55:45 +0100

    models

--- src/main/java/com/mahmoud/todo/models/AppUser.java
+++ src/main/java/com/mahmoud/todo/models/AppUser.java
@@ -1,5 +1,32 @@
 package com.mahmoud.todo.models;
 
-public class AppUser {
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+enum Role {
+    USER,
+    ADMIN
+}
+
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "username"))
+
+
+public class AppUser {
+    @Id
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
 }
---
+++ src/main/java/com/mahmoud/todo/models/Task.java
@@ -0,0 +1,30 @@
+package com.mahmoud.todo.models;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(nullable = false, updatable = false)
+    private String taskId;
+    @Column(nullable = false)
+    private String taskName;
+    private String taskDescription;
+    @Column(nullable = false, columnDefinition = "boolean default false")
+    private Boolean taskStatus=false;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "username")
+    private AppUser user;
+}
